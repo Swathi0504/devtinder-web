29b0ce507c13a32c4db1c4e8b5199832cb280b47
Swathi0504 <swathisivakumarbecse@gmail.com>
2025-02-19 22:13:06 +0530

    Login page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router'
 import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
+import axios from 'axios'
 
 const Body = () => {
-  return (<>
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser =async () => {
+    try {
+      const res = await axios.get(BASE_URL+"/profiles/view",{
+        withCredentials : true,
+      });
+
+      dispatch(addUser(res.data));
+    }
+    catch(err) {
+      if(err.status===401){
+        navigate("/login")
+      }
+      console.error(err);
+    }
+  }
+
+  useEffect(()=>{
+    fetchUser();
+  },[])
+
+  return (<div>
     <Navbar/>
     <Outlet/>
     <Footer/>
-  </>
+  </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router';
 
 const Navbar = () => {
 
@@ -7,7 +8,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-neutral-content">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">🖥️ DevTinder</a>
+    <Link to="/" className="btn btn-ghost text-xl">🖥️ DevTinder</Link>
   </div>
   {user && (<div className="flex-none gap-2">
     <p className='mr-4'>Welcome, {user.firstName}</p>
@@ -23,10 +24,9 @@ const Navbar = () => {
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to="/profile" className="justify-between">
             Profile
-            <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
